ba486313db46ece9df33c21e896b02ae2a3b10bb
adhilameenet <adhilameenet@gmail.com>
2023-01-19 16:09:45 +0530

    second

--- Java/Program 4/Armstrong.java
+++ Java/Program 4/Armstrong.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 class Armstrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num,temp;
-        System.out.print("Enter the Number :");
-        num = sc.nextInt();
-        temp = num;
-        while(num < 0) {
-            
+        int number,temp, remainder, result = 0;
+        System.out.print("Enter a Number :");
+        number = sc.nextInt();
+        temp = number;
+        while (number != 0)
+        {
+            remainder = number % 10;
+            result += Math.pow(remainder, 3);
+            number /= 10;
         }
+        if(result == temp)
+            System.out.println(temp + " is an Armstrong number.");
+        else
+            System.out.println(temp + " is not an Armstrong number.");
     }
 }
