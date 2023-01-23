4ac74f9feff0391468922496cad003c42a9b723f
adhilameenet <adhilameenet@gmail.com>
2023-01-23 15:26:17 +0530

    fix:thread

--- Java/Program 17/ThreadSample.java
+++ Java/Program 17/ThreadSample.java
@@ -1,6 +1,15 @@
-class Hi {
-    public void show() {
-        for (int i = 1; i <= 5; i++) {
+class ThreadSample {
+    public static void main(String []args) {
+        Hi ob1 = new Hi();
+        Hello ob2 = new Hello();
+        ob1.start();;
+        ob2.start();
+    }
+}
+
+class Hi extends Thread {
+    public void run() {
+        for(int i=1 ; i <= 5; i++) {
             System.out.println("Hi");
             try {
                 Thread.sleep(1000);
@@ -10,25 +19,14 @@ class Hi {
         }
     }
 }
-
-class Hello {
-    public void show() {
-        for (int i = 1; i <= 5; i++) {
+class Hello extends Thread {
+    public void run() {
+        for(int i=1; i <= 5; i++) {
             System.out.println("Hello");
             try {
                 Thread.sleep(1000);
             } catch (Exception e) {
-
+                
             }
         }
     }
-}
-
-class ThreadSample {
-    public static void main(String []args) {
-        Hi ob1 = new Hi();
-        Hello ob2 = new Hello();
-        ob1.show();;
-        ob2.show();
-    }
-}
